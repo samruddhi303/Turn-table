77241b9b68e466164629547ee59a6479364162ac
samruddhi303 <samruddhi1584@gmail.com>
2023-04-13 22:18:25 +0530

    commit on order

--- client/src/views/MyList/MyList.js
+++ client/src/views/MyList/MyList.js
@@ -1,27 +1,43 @@
 import React from "react";
+import axios from 'axios' 
+import swal from "sweetalert";
 import Navbar from "../../components/Navbar/Navbar"
 import { myFoodListItems } from "../../util/MyList";
+import { currentUser } from './../../util/currentUser'
 import "./MyList.css"
 
 
 function MyList() {
+    async function placeFoodOrder(){
+        const responce = await axios.post("/orderFoodItems", {
+            userId: currentUser._id,
+            tableNumber: localStorage.getItem("tableNumber") || 1, 
+            items: myFoodListItems
+
+        })
+       if (responce.data.success){
+        await swal("Order placed", responce.data.message, "success")
+       }
+    }
     return (
         <div>
             <Navbar/>
             <h1 className="text-center">MyList</h1>
+            <div className="Container">
             {
                 myFoodListItems.map((item, index)=>{
                     return(
                         <div>
                             <h6>Name: {item.name}</h6>
-                            <h6>Quantity: {item.Quantity}</h6>
+                            <h6>Quantity: {item.quantity}</h6>
                             <h6>price: {item.price}</h6>
                            <hr/>
                         </div>
                     )
                 })
             }
-            <button className="btn btn-primary">confirm order</button>
+            </div>
+            <button className=" btn btn-lg btn-outline-dark confirm-button " onClick={placeFoodOrder}>confirm order</button>
         </div>
 
     )
