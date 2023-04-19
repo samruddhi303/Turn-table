7e20b734b71c3f9a3b10bc2dae9988d24049eb96
samruddhi303 <samruddhi1584@gmail.com>
2023-04-19 23:19:13 +0530

    BookTable.js

--- client/src/views/BookTable/BookTable.js
+++ client/src/views/BookTable/BookTable.js
@@ -3,7 +3,8 @@ import React, { useEffect } from "react";
 import "./BookTable.css"
 
 import { loginRequired } from "../../util/loginRequired";
-
+import Navbar from "../../components/Navbar/Navbar";
+import {currentUser} from"../../util/currentUser";//fot taking Navber in this page
 function BookTable() {
 
 
@@ -20,14 +21,16 @@ function BookTable() {
 
     return (
         <div>
+            <Navbar user={currentUser?.name}/>
             <div className="container">
                 
-                   <div className="Tables"></div>
-                   <div className="Tables"></div>
-                   <div className="Tables"></div>
+                   <div className="Tables"><button class = "book">Book Table 1</button></div>
+                   <div className="Tables"><button class = "book">Book Table 2</button></div>
+                   <div className="Tables"><button class = "book">Book Table 3</button></div>
                    
 
                </div>
+               <div><button class ="confirmTable">Confirm Table</button></div>
          
         </div>
     )
